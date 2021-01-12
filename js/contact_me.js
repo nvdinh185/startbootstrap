4f86dd382aa3b074b9d7c3633d267e41128dba42
nvdinh185 <nvdinh0766777123@gmail.com>
2021-01-12 10:47:01 +0700

    ok

---
+++ js/contact_me.js
@@ -0,0 +1,86 @@
+$("#sendMessageButton").click(function () {
+    // get values from FORM
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var phone = $("input#phone").val();
+    var message = $("textarea#message").val();
+
+    $this = $("#sendMessageButton");
+    $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+    $.ajax({
+        url: "http://localhost:8080/test-post",
+        type: "POST",
+        data: {
+            name: name,
+            phone: phone,
+            email: email,
+            message: message,
+        },
+        datatype: "json",
+        cache: false,
+        success: function (data) {
+            console.log(data);
+            // Success message
+            $("#success").html("<div class='alert alert-success'>");
+            $("#success > .alert-success")
+                .html(
+                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                )
+                .append("</button>");
+            $("#success > .alert-success").append(
+                "<strong>Your message has been sent. </strong>"
+            );
+            $("#success > .alert-success").append("</div>");
+            //clear all fields
+            $("#contactForm").trigger("reset");
+        },
+        error: function () {
+            // Fail message
+            $("#success").html("<div class='alert alert-danger'>");
+            $("#success > .alert-danger")
+                .html(
+                    "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                )
+                .append("</button>");
+            $("#success > .alert-danger").append(
+                $("<strong>").text(
+                    "Sorry " +
+                    firstName +
+                    ", it seems that my mail server is not responding. Please try again later!"
+                )
+            );
+            $("#success > .alert-danger").append("</div>");
+            //clear all fields
+            $("#contactForm").trigger("reset");
+        },
+        complete: function () {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+        },
+    });
+})
+
+$("#getData").click(function () {
+    $this = $("#getData");
+    $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+    $.ajax({
+        url: "http://localhost:8080/test-get",
+        type: "GET",
+        cache: false,
+        success: function (data) {
+            // Success message
+            console.log(data);
+        },
+        error: function (err) {
+            // Fail message
+            console.log(err);
+        },
+        complete: function () {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+        },
+    });
+})
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
+});
