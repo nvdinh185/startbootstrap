49d302f2097e7cef46cf86a3c1fec2072fe3cd7a
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-07-30 08:30:26 +0700

    ok

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-fs = require('fs');
+const fs = require('fs');
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
